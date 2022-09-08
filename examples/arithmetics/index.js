4c8cf713be9219cabe86a4eecff82bc82eae689e
再见田园犬 <tcstory@163.com>
2022-09-08 16:57:44 +0800

    feat: 第二版的计算

--- examples/arithmetics/index.js
+++ examples/arithmetics/index.js
@@ -8,7 +8,7 @@ const {
 function run(node) {
   if (node instanceof Mul) {
     const leftVal = run(node.left)
-    const rightVal =  run(node.right)
+    const rightVal = run(node.right)
     if (node.operator === '*') {
       return leftVal * rightVal;
     } else {
@@ -16,7 +16,7 @@ function run(node) {
     }
   } else if (node instanceof Add) {
     const leftVal = run(node.left)
-    const rightVal =  run(node.right)
+    const rightVal = run(node.right)
     if (node.operator === '+') {
       return leftVal + rightVal;
     } else {
@@ -27,9 +27,37 @@ function run(node) {
   }
 }
 
-const out = parse(`(2 + 2) * 10`)
+function run2(node, cont) {
+  if (node instanceof Mul) {
+    return run2(node.left, function (leftVal) {
+      run2(node.right, function (rightVal) {
+        if (node.operator === '*') {
+          return cont(leftVal * rightVal);
+        } else {
+          return cont(leftVal / rightVal);
+        }
+      })
+    })
+  } else if (node instanceof Add) {
+    return run2(node.left, function (leftVal) {
+      run2(node.right, function (rightVal) {
+        if (node.operator === '+') {
+          return cont(leftVal + rightVal);
+        } else {
+          return cont(leftVal - rightVal);
+        }
+      })
+    })
+  } else {
+    cont(node.val)
+  }
+}
+
+const out = parse(`1+2+3+4+5`)
 console.log(
-  out,
-  run(out)
+  run(out),
+  run2(out, function (result) {
+    console.log(result)
+  })
 )
 
